f8303b53323f2310d207b3a7853dc9ba38400521
Ruslan Saifullin <uraltus@yandex.ru>
2020-03-07 19:36:57 +0500

    Updates

---
+++ precache-manifest.1091f47a4e0028e336eee59c48d9d55b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe23370402d33808db0fe58db4e58c8",
+    "url": "/ruslan-mini-app/index.html"
+  },
+  {
+    "revision": "8f304bbf513becfbb2c0",
+    "url": "/ruslan-mini-app/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "03e0b6ed7a880191d0ed",
+    "url": "/ruslan-mini-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8f304bbf513becfbb2c0",
+    "url": "/ruslan-mini-app/static/js/2.ef0e51b6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ruslan-mini-app/static/js/2.ef0e51b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e0b6ed7a880191d0ed",
+    "url": "/ruslan-mini-app/static/js/main.254e6954.chunk.js"
+  },
+  {
+    "revision": "8080dda49fcae09c276a",
+    "url": "/ruslan-mini-app/static/js/runtime-main.81e67440.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/ruslan-mini-app/static/media/persik.4e1ec840.png"
+  }
+]);
